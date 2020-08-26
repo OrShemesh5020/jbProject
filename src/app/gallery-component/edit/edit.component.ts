ae0839e6cb86724664ca57674f9b9186c78d9f70
OrShemesh <orshemesh5020@gmail.com>
2020-08-26 23:23:49 +0300

    form

--- src/app/gallery-component/edit/edit.component.ts
+++ src/app/gallery-component/edit/edit.component.ts
@@ -1,7 +1,9 @@
+import { URLValidator } from './../../shared/validators/validator';
 import { GalleryElement } from './../gallery-component.model';
 import { GalleryService } from './../../gallery.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Routes, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
@@ -10,11 +12,41 @@ import { ActivatedRoute, Routes, Router } from '@angular/router';
 })
 export class EditComponent implements OnInit {
   model: GalleryElement;
+  editForm: FormGroup;
   constructor(
     private activated: ActivatedRoute,
     private galleryService: GalleryService,
-    private routed: Router
-  ) {}
+    private routed: Router,
+    private formBuilder: FormBuilder
+  ) {
+    this.editForm = this.formBuilder.group({
+      title: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(15),
+        ],
+      ],
+      description: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(400),
+        ],
+      ],
+      src: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(7),
+          Validators.maxLength(400),
+          URLValidator.prefix(),
+        ],
+      ],
+    });
+  }
 
   ngOnInit(): void {
     this.activated.params.subscribe(
@@ -23,6 +55,14 @@ export class EditComponent implements OnInit {
     // console.log(this.model);
   }
   onSubmit(): void {
+    // console.log(this.editForm);
+    // console.log(this.editForm.value.title);
+    this.editModel();
     this.routed.navigate(['gallery/view']);
+  }
+  editModel(): void {
+    this.model.title = this.editForm.value.title;
+    this.model.src = this.editForm.value.src;
+    this.model.description = this.editForm.value.description;
   }
 }
